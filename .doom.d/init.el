8df9bf7e591cb971bbfe7d97e9d9b7e00c7aebb1
Marino Skalnik <marino.skalnik@gmail.com>
2023-01-21 23:37:25 +0100

    Remove unused languages from doom emacs

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -140,14 +140,14 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json                ; At least it ain't XML
-       (java +lsp)         ; the poster child for carpal tunnel syndrome
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        kotlin              ; a better, slicker Java(Script)
-       latex               ; writing papers in Emacs has never been so fun
+       ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       lua                 ; one-based indices? one-based indices
+       ;;lua               ; one-based indices? one-based indices
        markdown            ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -155,7 +155,7 @@
        org                 ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
-       purescript          ; javascript, but functional
+       ;;purescript        ; javascript, but functional
        python              ; beautiful is better than ugly
        qt                  ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs

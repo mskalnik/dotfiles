3bde1b1fadcb80892086fd324097f0c8fde6f7e8
Marino Skalnik <marino.skalnik@gmail.com>
2023-10-15 23:44:19 +0200

    Add date to the install script logs

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 backup_dotfiles() {
-    echo "-[START] Backing up dotfiles"
+    echo "-[START] Backing up dotfiles - $(date)"
     DOTFILES_DIR="$HOME/dotfiles"
     BACKUP_DIR="$HOME/dotfiles_backup_$(date +'%Y%m%d%H%M%S')"
     mkdir -p "$BACKUP_DIR"
@@ -12,28 +12,28 @@ backup_dotfiles() {
             echo "Backed up $dotfile to $BACKUP_DIR"
         fi
     done
-    echo "-[FINISH] Backing up dotfiles"
+    echo "-[FINISH] Backing up dotfiles - $(date)"
 }
 
 install_packages() {
-    echo "-[START] Installing packages"
+    echo "-[START] Installing packages - $(date)"
     brew install -y cairo openjdk sbt
-    echo "-[FINISH] Installing packages"
+    echo "-[FINISH] Installing packages - $(date)"
 }
 
 setup_ohmyzsh() {
-    echo "-[START] Setting up oh my zsh"
+    echo "-[START] Setting up oh my zsh - $(date)"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     chsh -s /opt/homebrew/bin/zsh
-    echo "-[FINISH] Setting up oh my zsh"
+    echo "-[FINISH] Setting up oh my zsh - $(date)"
 }
 
 main() {
-    echo "[START] Installing dotfiles"
+    echo "[START] Installing dotfiles - $(date)"
     backup_dotfiles
     install_packages
     setup_ohmyzsh
-    echo "[FINISH] Installing dotfiles"
+    echo "[FINISH] Installing dotfiles - $(date)"
 }
 
 main

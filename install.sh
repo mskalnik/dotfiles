c8586441ff69a2fc53b26ccf5c819b6e49e04eeb
Marino Skalnik <marino.skalnik@gmail.com>
2023-12-09 23:35:32 +0100

    Add error handling in install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+DOTFILES_DIR="$HOME/dotfiles"
+BACKUP_DIR="$HOME/dotfiles_backup_$(date +'%Y%m%d%H%M%S')"
+
 backup_dotfiles() {
     echo "|-[START] Backing up dotfiles - $(date)"
-    DOTFILES_DIR="$HOME/dotfiles"
-    BACKUP_DIR="$HOME/dotfiles_backup_$(date +'%Y%m%d%H%M%S')"
-    mkdir -p "$BACKUP_DIR"
+    mkdir -p "$BACKUP_DIR" || { echo "Error creating backup directory"; exit 1; }
 
     for dotfile in "${DOTFILES[@]}"; do
         if [ -f "$HOME/$dotfile" ]; then
-            mv "$HOME/$dotfile" "$BACKUP_DIR"
+            mv "$HOME/$dotfile" "$BACKUP_DIR" || { echo "Error backing up $dotfile"; exit 1; }
             echo "Backed up $dotfile to $BACKUP_DIR"
         fi
     done
@@ -17,14 +18,14 @@ backup_dotfiles() {
 
 install_packages() {
     echo "|-[START] Installing packages - $(date)"
-    brew install -y cairo openjdk sbt
+    brew install -y cairo openjdk sbt || { echo "Error installing packages"; exit 1; }
     echo "|-[FINISH] Installing packages - $(date)"
 }
 
 setup_ohmyzsh() {
     echo "|-[START] Setting up oh my zsh - $(date)"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    chsh -s /opt/homebrew/bin/zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || { echo "Error setting up Oh My Zsh"; exit 1; }
+    chsh -s /opt/homebrew/bin/zsh || { echo "Error changing shell"; exit 1; }
     echo "|-[FINISH] Setting up oh my zsh - $(date)"
 }
 
